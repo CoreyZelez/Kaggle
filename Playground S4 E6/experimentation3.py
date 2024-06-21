fe52f8503be3db537c7cb624ec1b1657d9941ddb
Corey Zelez <coreyzelez@gmail.com>
2024-06-21 17:01:03 +1000

    Added experimentation file.

---
+++ Playground S4 E6/experimentation3.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler
